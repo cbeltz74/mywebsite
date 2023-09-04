5bca4f70d14493a127f334f49b15aa4fe7d94621
Colin Beltz <colinbeltz@gmail.com>
2023-09-03 20:53:39 -0500

    Added Quiz file structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { Header } from "./Header"
 export { Footer } from "./Footer"
 export {ProjectCard } from "./ProjectCard"
+
+export {EndScreen} from "./QuizComponents/EndScreen"
+export {MainMenu} from "./QuizComponents/MainMenu"
+export {Quiz} from "./QuizComponents/Quiz"
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,14 +3,6 @@ import { ProjectCard } from "../components";
 
 export const Projects = () => {
 
-// const [projects, setProjects] = useState([]);
-
-// useEffect(() => {
-//   fetch('http://localhost:8000/projects')
-// .then(response => response.json())
-// .then((data => console.log(data)));
-// }, []);
-
   return (
     <main>
       <h1>Projects</h1>
